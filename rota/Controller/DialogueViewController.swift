a1cb0843bd93a4b4c764cfa14c024f225552ad28
riku arakawa <a_ra_ra_rikky0611@yahoo.co.jp>
2017-10-22 16:42:43 +0900

    Implement JSQMessagesViewController in DialogueViewController

--- rota/Controller/DialogueViewController.swift
+++ rota/Controller/DialogueViewController.swift
@@ -7,18 +7,109 @@
 //
 
 import UIKit
+import JSQMessagesViewController
 
-class DialogueViewController: UIViewController {
+struct User {
+    let id: String
+    let name: String
+}
+
+class DialogueViewController: JSQMessagesViewController {
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    let user1 = User(id: "1", name: "doctoe")
+    let user2 = User(id: "2", name: "yoshimaro")
+    
+    var currentUser: User {
+        return user1
     }
     
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
+    var messages = [JSQMessage]()
     
 }
+
+extension DialogueViewController {
+    
+    //送信ボタンが押された時の挙動
+    override func didPressSend(_ button: UIButton!, withMessageText text: String!, senderId: String!, senderDisplayName: String!, date: Date!) {
+        let message = JSQMessage(senderId: senderId, displayName: senderDisplayName, text: text)
+        
+        messages.append(message!)
+        
+        finishSendingMessage()
+    }
+    
+    //添付ファイルボタンが押された時の挙動
+    override func didPressAccessoryButton(_ sender: UIButton!) {
+        
+    }
+    
+    //各送信者の表示について
+    override func collectionView(_ collectionView: JSQMessagesCollectionView!, attributedTextForMessageBubbleTopLabelAt indexPath: IndexPath!) -> NSAttributedString! {
+        let message = messages[indexPath.row]
+        let messageUsername = message.senderDisplayName
+        
+        return NSAttributedString(string: messageUsername!)
+    }
+    
+    //各メッセージの高さ
+    override func collectionView(_ collectionView: JSQMessagesCollectionView!, layout collectionViewLayout: JSQMessagesCollectionViewFlowLayout!, heightForMessageBubbleTopLabelAt indexPath: IndexPath!) -> CGFloat {
+        return 15
+    }
+    
+    //各送信者の表示に画像を使うか
+    override func collectionView(_ collectionView: JSQMessagesCollectionView!, avatarImageDataForItemAt indexPath: IndexPath!) -> JSQMessageAvatarImageDataSource! {
+        return nil
+    }
+    
+    //各メッセージの背景を設定
+    override func collectionView(_ collectionView: JSQMessagesCollectionView!, messageBubbleImageDataForItemAt indexPath: IndexPath!) -> JSQMessageBubbleImageDataSource! {
+        let bubbleFactory = JSQMessagesBubbleImageFactory()
+        
+        let message = messages[indexPath.row]
+        
+        if currentUser.id == message.senderId {
+            return bubbleFactory?.outgoingMessagesBubbleImage(with: .green)
+        } else {
+            return bubbleFactory?.incomingMessagesBubbleImage(with: .blue)
+        }
+    }
+    
+    //   メッセージの総数を取得
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return messages.count
+    }
+    
+    //   メッセージの内容参照場所の設定
+    override func collectionView(_ collectionView: JSQMessagesCollectionView!, messageDataForItemAt indexPath: IndexPath!) -> JSQMessageData! {
+        return messages[indexPath.row]
+    }
+}
+
+extension DialogueViewController {
+    //   画面を開いた直後の挙動。ここで使用者側の設定を行ない、過去のメッセージを取得している
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.senderId = currentUser.id
+        self.senderDisplayName = currentUser.name
+        
+        //   メッセージ取得の関数（真下）呼び出し
+        self.messages = getMessages()
+    }
+}
+
+extension DialogueViewController {
+    //   テストメッセージ作成、呼び出し関数作成
+    func getMessages() -> [JSQMessage] {
+        var messages = [JSQMessage]()
+        
+        let message1 = JSQMessage(senderId: "1", displayName: "yatsure", text: "こんにちわ")
+        let message2 = JSQMessage(senderId: "2", displayName: "yoshimaro", text: "せやかて工藤")
+        
+        messages.append(message1!)
+        messages.append(message2!)
+        
+        return messages
+    }
+}
+
